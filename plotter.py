70cd0fb13017ae129596b50fc68c473583b20d5a
Arthur Allshire <arthur.allshire@gmail.com>
2017-03-25 15:13:17 +1100

    Add basic log plotting script

---
+++ plotter.py
@@ -0,0 +1,27 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+
+last = None
+
+fname = "1490415085.csv"
+vars_to_plt = ["sin_time", "cos_time"]
+
+data = {}
+with open(fname, 'rU') as infile:
+  # read the file as a dictionary for each row ({header : value})
+  reader = csv.DictReader(infile)
+  for row in reader:
+    for header, value in row.items():
+      try:
+        data[header].append(value)
+      except KeyError:
+        data[header] = [value]
+
+plt.figure("Robot Log Plotter")
+for i, var in enumerate(vars_to_plt):
+    plt.subplot(211+i)
+    plt.ylabel(var)
+    times = data["time"]
+    plt.plot(times, data[var], 'bo')
+plt.show()
